93eee75cd621b099f68610c64d237720561cd3c5
owayo <owayopaul@gmail.com>
2025-04-26 17:03:00 +0300

    feat: refactor main.tsx to integrate Redux and React Router with axios setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes';
+import axios from 'axios';
+import { Provider } from 'react-redux';
+import { store } from './store/Store';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+/**setup axios */
+axios.defaults.baseURL = "https://api.themoviedb.org/3"
+axios.defaults.headers.common['Authorization'] = `Bearer ${import.meta.env.VITE_REACT_APP_ACCESS_TOKEN}`
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+    throw new Error("Root element not found");
+}
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+    <Provider store={store}>
+          <RouterProvider router={router}/>
+    </Provider>
+);
