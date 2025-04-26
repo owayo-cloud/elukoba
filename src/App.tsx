a7a0136c82b9c6829f53a4580cb6a18a5e8bf1ef
owayo <owayopaul@gmail.com>
2025-04-26 16:55:24 +0300

    feat: enhance App component with data fetching and layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,51 @@
-import './App.css'
+import { Outlet } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MobileNav from './components/MobileNav';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setBannerData,setImageURL } from './store/LukobaSlice';
 
 function App() {
+  const dispatch = useDispatch()
+
+  const fetchTrendingData = async()=>{
+    try {
+        const response = await axios.get('/trending/all/week')
+
+        dispatch(setBannerData(response.data.results))
+    } catch (error) {
+        console.log("error",error)
+    }
+  }
+
+  const fetchConfiguration = async()=>{
+    try {
+        const response = await axios.get("/configuration")
+
+        dispatch(setImageURL(response.data.images.secure_base_url+"original"))
+    } catch (error) {
+      
+    }
+  }
+
+  useEffect(()=>{
+    fetchTrendingData()
+    fetchConfiguration()
+  },[])
+  
   return (
-    <div className='text-center text-2xl font-bold text-blue-500'>
-      Main Page
-    </div>
-  )
+    <main className='pb-14 lg:pb-0'>
+        <Header/>
+        <div className='min-h-[90vh]'>
+            <Outlet/>
+        </div>
+        <Footer/>
+        <MobileNav/>
+    </main>
+  );
 }
 
-export default App
+export default App;
